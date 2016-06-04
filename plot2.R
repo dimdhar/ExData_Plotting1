4b645e7517ddac9eb46be635e2fa166872323fd5
Dimple Dhar <ddhar@icwgrp.com>
2016-06-04 16:07:05 -0700

    Adding R and Png files

---
+++ plot2.R
@@ -0,0 +1,19 @@
+
+## Get all into a dataset
+data_all <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Subsetting the data to the dates from 2007-02-01 to 2007-02-02
+data2007 <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_all)
+
+## Converting dates to deisred format
+datetime <- paste(as.Date(data2007$Date), data2007$Time)
+data2007$Datetime <- as.POSIXct(datetime)
+
+## Plot 
+plot(data2007$Global_active_power~data2007$Datetime, type="l",    ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving to file  plot2.png
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
